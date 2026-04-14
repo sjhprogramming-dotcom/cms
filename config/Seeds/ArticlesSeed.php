9199143e03e01215a2fa10c1788fd4754f1fd068
steve houldey <sjhprogramming@gmail.com>
2026-04-14 20:46:50 +0100

    Added Migrations and Seeds for Users, Articles, Tags, and ArticlesTags tables.

---
+++ config/Seeds/ArticlesSeed.php
@@ -0,0 +1,56 @@
+<?php
+declare(strict_types=1);
+
+use Migrations\BaseSeed;
+
+/**
+ * Articles seed.
+ */
+class ArticlesSeed extends BaseSeed
+{
+    /**
+     * Run Method.
+     *
+     * Write your database seeder using this method.
+     *
+     * More information on writing seeds is available here:
+     * https://book.cakephp.org/migrations/4/en/seeding.html
+     *
+     * @return void
+     */
+    public function run(): void
+    {
+        $data = [
+            [
+                'user_id' => 1,
+                'title' => 'First Post',
+                'slug' => 'first-post',
+                'body' => 'This is the first post.',
+                'published' => true,
+                'created' => date('Y-m-d H:i:s'),
+                'modified' => date('Y-m-d H:i:s'),
+            ],
+            [
+                'user_id' => 2,
+                'title' => 'Second Post',
+                'slug' => 'second-post',
+                'body' => 'This is the second post.',
+                'published' => true,
+                'created' => date('Y-m-d H:i:s'),
+                'modified' => date('Y-m-d H:i:s'),
+            ],
+            [
+                'user_id' => 2,
+                'title' => 'Third Post',
+                'slug' => 'third-post',
+                'body' => 'This is the 3rd post.',
+                'published' => true,
+                'created' => date('Y-m-d H:i:s'),
+                'modified' => date('Y-m-d H:i:s'),
+            ],
+        ];
+
+        $table = $this->table('articles');
+        $table->insert($data)->save();
+    }
+}
---
+++ config/Seeds/UsersSeed.php
@@ -0,0 +1,41 @@
+<?php
+declare(strict_types=1);
+
+use Migrations\BaseSeed;
+
+/**
+ * Users seed.
+ */
+class UsersSeed extends BaseSeed
+{
+    /**
+     * Run Method.
+     *
+     * Write your database seeder using this method.
+     *
+     * More information on writing seeds is available here:
+     * https://book.cakephp.org/migrations/4/en/seeding.html
+     *
+     * @return void
+     */
+    public function run(): void
+    {
+        $data = [
+            [
+                'email' => 'cakephp@example.com',
+                'password' => 'secret',
+                'created' => date('Y-m-d H:i:s'),
+                'modified' => date('Y-m-d H:i:s'),
+            ],
+            [
+                'email' => 'steve@sjh.com',
+                'password' => 'secret',
+                'created' => date('Y-m-d H:i:s'),
+                'modified' => date('Y-m-d H:i:s'),
+            ],
+        ];
+
+        $table = $this->table('users');
+        $table->insert($data)->save();
+    }
+}
