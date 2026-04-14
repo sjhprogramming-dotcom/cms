3fd7736b695acafe24d24fea254bdd4fb69bd8b3
steve houldey <sjhprogramming@gmail.com>
2026-04-14 20:50:45 +0100

    Built the Views ahead of my first Video Tutorial

---
+++ tests/Fixture/UsersFixture.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Test\Fixture;
+
+use Cake\TestSuite\Fixture\TestFixture;
+
+/**
+ * UsersFixture
+ */
+class UsersFixture extends TestFixture
+{
+    /**
+     * Init method
+     *
+     * @return void
+     */
+    public function init(): void
+    {
+        $this->records = [
+            [
+                'id' => 1,
+                'email' => 'Lorem ipsum dolor sit amet',
+                'password' => 'Lorem ipsum dolor sit amet',
+                'created' => '2026-04-14 19:49:23',
+                'modified' => '2026-04-14 19:49:23',
+            ],
+        ];
+        parent::init();
+    }
+}
